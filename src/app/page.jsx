a98f987d459a549fa67f342dbeb698f197fe469b
eman-a-dev <emanarif149@gmail>
2024-03-21 21:39:31 +0500

    my app

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
-
+import Banner from "/components/banner";
+import Form from "/components/form";
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <>
+      <Banner />
+      <Form />
+    </>
   );
 }
